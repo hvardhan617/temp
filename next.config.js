8a07f1fd2225854c8d1ecb0a73bcf28cd9ccb242
Harsha Vardhan <harsha@Harshas-MacBook-Pro.local>
2023-03-25 10:11:19 +0530

    REmove unused imports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: 'a/fibr',
+  basePath: '/a/fibr',
+  assetPrefix: '/a/fibr/',
 }
 
 module.exports = nextConfig
