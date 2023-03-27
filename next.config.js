47d72ebda6126d44b31aa42e1d3121e8ca2a919b
Harsha Vardhan <harsha@Harshas-MacBook-Pro.local>
2023-03-27 10:26:28 +0530

    Vercel rewrites

--- next.config.js
+++ next.config.js
@@ -1,33 +1,40 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
+    const rewrites = [
+      // {
+      //   "source": "/:path*",
+      //   "destination": "/_next/:path*",
+      //   "has": [
+      //     {
+      //       "type": "query",
+      //       "key": "pathname",
+      //       "value": "/_next/"
+      //     }
+      //   ]
+      // },
+      {
+        source: '/a/fibr/_next/static/:path*',
+        destination: '/_next/static/:path*',
+      },
       {
         source: '/a/fibr/:path*',
         destination: '/:path*',
-      },
+      },  
     ];
+  
+    // Loop through each rewrite and log input/output
+    rewrites.forEach((rewrite) => {
+      const { source, destination } = rewrite;
+      console.log(`Rewriting "${source}" to "${destination}"`);
+    });
+  
+    return rewrites;
   },
+  
   reactStrictMode: true,
-  distDir: 'a/fibr',
-  // basePath: '/a/fibr',
-  assetPrefix: 'a/fibr',
-  // webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-  //   // Add a rule to handle custom fonts and change the output path
-  //   config.module.rules.push({
-  //     test: /\.(woff|woff2|eot|ttf|otf)$/,
-  //     use: {
-  //       loader: 'file-loader',
-  //       options: {
-  //         publicPath: '/_next/static/media/',
-  //         outputPath: '/a/fibr/_next/static/media/',
-  //         name: '[name]-[hash].[ext]',
-  //       },
-  //     },
-  //   });
+  distDir: '/a/fibr',
+  assetPrefix: '/a/fibr',
+};
 
-  //   return config;
-  // },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
