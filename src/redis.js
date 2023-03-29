936ba96c3bae66d750686c9428700be54fabbc0b
Harsha Vardhan <harsha@Harshas-MacBook-Pro.local>
2023-03-30 00:40:59 +0530

    Add redis caching

--- src/redis.js
+++ src/redis.js
@@ -1,7 +1,7 @@
 import * as redis from 'redis';
 
 const client = redis.createClient({
-    host: '3.108.105.59', // replace with your Redis server host
+    host: process.env.NEXT_PUBLIC_CACHE_HOST, // replace with your Redis server host
     port: 6379 // replace with your Redis server port
 });
 client
